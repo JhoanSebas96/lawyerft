fbf2c7b6bb2564720d9a909a4326f3fbfaa2d52b
SebastianHernandez <38589472+JhoanSebas96@users.noreply.github.com>
2024-04-11 19:37:42 -0500

    modified "area de actuacion"

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,12 +20,16 @@ function Home() {
       <div className='section-area'>
         <div className='area'>
           <h2 className='area-title'>areas de actuación</h2>
-          <div className='area-des'>
-            <div>01.</div>
-            <h3>title</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia saepe ea neque nihil placeat possimus inventore reprehenderit quibusdam maiores sequi. Doloribus alias quia maiores vero porro blanditiis quas deserunt dolore.</p>
-            <div>icon</div>
-          </div>
+          {areas.map((area, index) =>{
+            return(
+              <div className='area-des'>
+                <div>0{index + 1}.</div>
+                <h3>{area.title}</h3>
+                <p>{area.description}</p>
+                <div>icon</div>
+              </div>
+            )
+          })}
         </div>
       </div>
     </>
@@ -34,20 +38,20 @@ function Home() {
 
 export default Home
 
-const infoText = "Somos una firma de abogados dedicados a una profunda investigación de los contextos jurídicos de nuestros clientes, lo que nos permiten lograr con anticipación la solución de conflictos, generando resultados garantizados por nuestros expertos en las diferentes áreas jurídicas, a fin de salvaguardar y proteger sus derechos con respeto y ética por la justicia, encajando así nuestro mejor rol con social."
+const infoText = "Somos una firma de abogados dedicados a una profunda investigación de los contextos jurídicos de nuestros clientes, lo que nos permiten \n lograr con anticipación la solución de conflictos, generando resultados garantizados por nuestros expertos en las diferentes áreas jurídicas, a fin de salvaguardar y proteger sus derechos con respeto y ética por la justicia, encajando así nuestro mejor rol con social."
 
 const areas = [
   {
     title: 'derecho civil',
-    description: 'Redacción, revisión y negociación de contratos. \nAsesoramiento en materia de responsabilidad civil. \nTramitación de divorcios, separaciones, herencias y otros asuntos de derecho familiar. \nRepresentación en casos de propiedad horizontal y arrendamientos.'
+    description: 'Redacción, revisión y negociación de contratos. \n Asesoramiento en materia de responsabilidad civil. \n Tramitación de divorcios, separaciones, herencias y otros asuntos de derecho familiar. \n Representación en casos de propiedad horizontal y arrendamientos.'
   },
   {
     title: 'derecho civil',
-    description: 'Defensa y acusación en delitos de diversa índole. \nAsistencia a víctimas de violencia de género. \nRepresentación legal de menores infractores.\nAsesoramiento en materia penitenciaria.'
+    description: 'Defensa y acusación en delitos de diversa índole. \n Asistencia a víctimas de violencia de género. \n Representación legal de menores infractores.\n Asesoramiento en materia penitenciaria.'
   },
   {
     title: 'derecho civil',
-    description: 'Impugnación de despidos y reclamaciones de salarios. \n Redacción, revisión y negociación de contratos de trabajo. \n Asesoramiento en materia de Seguridad Social. \nRepresentación en casos de huelgas y conflictos colectivos.'
+    description: `Impugnación de despidos y reclamaciones de salarios. \n Redacción, revisión y negociación de contratos de trabajo. \n Asesoramiento en materia de Seguridad Social. \n Representación en casos de huelgas y conflictos colectivos.`
   },
   {
     title: 'derecho civil',
